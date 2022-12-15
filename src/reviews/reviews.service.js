caeb004655f61a5e4d6bd79889c5abbe55df243e
unknown <ndiayembayesamb@gmail.com>
2022-12-14 22:18:33 -0800

    final

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -18,7 +18,7 @@ async function update(review) {
     const critic = await db("reviews as r").join("critics as c", "c.critic_id", "r.critic_id").select("c.*")
         .where({ "r.critic_id": review.critic_id }).first()
 
-    await db("reviews").where({ review_id: review.review_id }).update(review) //1
+    await db("reviews").where({ review_id: review.review_id }).update(review)
 
     const [updated] = await db("reviews").select("*").where({ review_id: review.review_id })
 
